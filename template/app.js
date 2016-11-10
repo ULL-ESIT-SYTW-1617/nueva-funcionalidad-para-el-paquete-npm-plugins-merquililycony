36a62a58d82085937e06a60c12e4802c7fc641da
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-11-10 21:36:05 +0000

    Cambios app.js

--- template/app.js
+++ template/app.js
@@ -6,7 +6,7 @@ var app = express();
 var path = require('path');
 
 var exec = require('child_process').exec;
-
+var  expressEJS = require('express-ejs-layouts')
 
    // ruteo
 
@@ -18,7 +18,7 @@ app.get('/', function(request, response) {
 });
 
 
-app.post('/sync', function(request, response) {
+/*app.post('/sync', function(request, response) {
         function puts(error, stdout, stderr) {
            console.log("Salida:"+stdout);
            if(error) console.log("Error:"+error);
@@ -27,14 +27,15 @@ app.post('/sync', function(request, response) {
    exec('git clone https://github.com/ULL-ESIT-SYTW-1617/nueva-funcionalidad-para-el-paquete-npm-plugins-merquililycony');
    exec("git pull https://github.com/ULL-ESIT-SYTW-1617/nueva-funcionalidad-para-el-paquete-npm-plugins-merquililycony.git  master", puts);
    response.redirect('/');
-});
+});*/
 
 
     // escuchar
    // app.listen(8080);
 app.set('port', process.env.PORT || 8080);
 
-
+app.use(expressEJS);
+app.use(express.static(path.join(__dirname,'gh-pages')));
 //heroku git:remote -a heroku-project-name
 app.listen(app.get('port'), function() {
   console.log('Servidor escuchando en el puerto:'+app.get('port'));
