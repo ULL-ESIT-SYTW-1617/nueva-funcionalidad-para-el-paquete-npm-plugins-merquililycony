7856843d883c5eb857c6a3205ee229a013d3da50
Cony <alu0100673647@ull.edu.es>
2016-11-26 18:40:41 +0000

    Quitando tareas sobrantes

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -60,18 +60,3 @@ gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/losh generate-gitbook']));
 });
 
-
-
-gulp.task('instalar_recursos',['instalar_dependencias','instalar_plugins']);
-
-gulp.task('instalar_dependencias', function()
-{
-    gulp.src(['./package.json']).pipe(install())
-});
-
-gulp.task('instalar_plugins', function()
-{
-    return gulp.src('').pipe(shell([
-        'gitbook install'    
-    ])) 
-});
